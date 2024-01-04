ea5d7511582595ff205443dd8fbd3d52003706a1
Sousa <telmo_sousa2003@hotmail.com>
2024-01-04 03:07:38 +0000

    changed default grid size

--- src/org/codeforall/ooptimus/Main.java
+++ src/org/codeforall/ooptimus/Main.java
@@ -3,7 +3,7 @@ package org.codeforall.ooptimus;
 public class Main {
     public static void main(String[] args) {
 
-        Game g = new Game(GridType.SIMPLE_GFX,6,6);
+        Game g = new Game(GridType.SIMPLE_GFX,40,20);
         System.out.println("COMMANDS: \n h left \n j down \n k up \n l right \n i paint \n r cycle color \n u clean all");
         g.init();
 
